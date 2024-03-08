a0e7d6afd7a67287856f57be3cd12cf0c6722d75
eminorhan <aeminorhan@gmail.com>
2024-03-07 20:37:56 -0500

    minor bug fix: add forgotten epoch argument in display forward pass

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ A new simple discrete autoencoder model.
 
 - Encoder and decoder are both ViT models.
 - Output of encoder at each patch is a one-hot vector with size `vocab_size`.
-- Gradient is estimated with the straight-through estimator at this discretization.
+- Gradient is estimated with the straight-through estimator at this discretization step.
 - The skeleton of the code is recycled from Facebook's [MAE](https://github.com/facebookresearch/mae) repository with several simplifications.
--- train.py
+++ train.py
@@ -93,7 +93,7 @@ def main(args):
     if args.compile:
         model = torch.compile(model)
 
-    model = DDP(model, device_ids=[args.gpu], find_unused_parameters=True)  # TODO: try FSDP
+    model = DDP(model, device_ids=[args.gpu])  # TODO: try FSDP
     model_without_ddp = model.module
     
     print(f"Model: {model_without_ddp}")
@@ -165,7 +165,7 @@ def main(args):
         if args.display:
             samples = samples.to(device, non_blocking=True)
             with torch.cuda.amp.autocast():
-                _, pred = model(samples[:8, ...])
+                _, pred = model(samples[:8, ...], epoch+1)
                 pred = model_without_ddp.unpatchify(pred)
             combined = torch.cat((samples[:8, ...], pred), 0)
             save_image(combined, f"{args.save_prefix}_reconstructions_epoch_{epoch}.jpg", nrow=8, padding=1, normalize=True, scale_each=True)
