29656ca0675ed64f0da8975ce42d0e42500684d0
eminorhan <aeminorhan@gmail.com>
2024-05-03 12:42:02 -0400

    remove non-wds training code

--- train.py
+++ train.py
@@ -14,13 +14,14 @@ import math
 import argparse
 import json
 from pathlib import Path
+import webdataset as wds
 import torch
 print(torch.__version__)
 from numpy import mean as npmean
 import torch.backends.cudnn as cudnn
 import torchvision.transforms as transforms
 from torchvision.datasets import ImageFolder
-from torch.utils.data import DataLoader, DistributedSampler, SequentialSampler
+from torch.utils.data import DataLoader, SequentialSampler
 from torch.nn.parallel import DistributedDataParallel as DDP
 from torchvision.utils import save_image
 
@@ -30,33 +31,31 @@ from util.misc import NativeScalerWithGradNormCount as NativeScaler
 
 
 def get_args_parser():
-    parser = argparse.ArgumentParser('TAE training', add_help=False)
+    parser = argparse.ArgumentParser('TAE training with webdataset', add_help=False)
     parser.add_argument('--batch_size_per_gpu', default=256, type=int, help='Batch size per GPU (effective batch size is batch_size_per_gpu * accum_iter * # gpus')
-    parser.add_argument('--epochs', default=999999, type=int)
     parser.add_argument('--accum_iter', default=1, type=int, help='Accumulate gradient iterations (for increasing the effective batch size under memory constraints)')
-    parser.add_argument("--save_prefix", default="", type=str, help="""prefix for saving checkpoint and log files""")
+    parser.add_argument('--save_prefix', default="", type=str, help="""prefix for saving checkpoint and log files""")
+    parser.add_argument('--save_freq', default=10000, type=int, help='Save checkpoint every this many iterations.')
 
     # Model parameters
     parser.add_argument('--model', default='', type=str, help='Name of model to train')
     parser.add_argument('--resume', default='', help='resume from a checkpoint')
     parser.add_argument('--input_size', default=224, type=int, help='images input size')
     parser.add_argument('--compile', action='store_true', help='whether to compile the model for improved efficiency (default: false)')
-    parser.add_argument('--display', action='store_true', help='whether to display reconstruction at the end of each epoch.')
+    parser.add_argument('--display', action='store_true', help='whether to display reconstruction at regular intervals.')
 
     # Optimizer parameters
     parser.add_argument('--weight_decay', type=float, default=0.05, help='weight decay (default: 0.05)')
     parser.add_argument('--lr', type=float, default=0.0001, help='learning rate (absolute lr)')
-    parser.add_argument('--min_lr', type=float, default=0.0001, help='lower lr bound for cyclic schedulers that hit 0')
 
     # Dataset parameters
     parser.add_argument('--train_data_path', default='', type=str)
     parser.add_argument('--val_data_path', default='', type=str)
     parser.add_argument('--output_dir', default='./output_dir', help='path where to save, empty for no saving')
     parser.add_argument('--device', default='cuda', help='device to use for training/testing')
-    parser.add_argument('--start_epoch', default=0, type=int, help='start epoch')
     parser.add_argument('--num_workers', default=16, type=int)
-    parser.add_argument("--jitter_scale", default=[0.2, 1.0], type=float, nargs="+")
-    parser.add_argument("--jitter_ratio", default=[3.0/4.0, 4.0/3.0], type=float, nargs="+")
+    parser.add_argument('--jitter_scale', default=[0.2, 1.0], type=float, nargs="+")
+    parser.add_argument('--jitter_ratio', default=[3.0/4.0, 4.0/3.0], type=float, nargs="+")
 
     # distributed training parameters
     parser.add_argument('--dist_url', default='env://', help='url used to set up distributed training')
@@ -89,16 +88,14 @@ def main(args):
     ])
 
     # train and val datasets and loaders
-    train_dataset = ImageFolder(args.train_data_path, transform=train_transform)
-    train_sampler = DistributedSampler(train_dataset, num_replicas=misc.get_world_size(), rank=misc.get_rank(), shuffle=True)
-    train_loader = DataLoader(train_dataset, sampler=train_sampler, batch_size=args.batch_size_per_gpu, num_workers=args.num_workers, pin_memory=True, drop_last=True)
+    train_dataset = wds.WebDataset(args.train_data_path, resampled=True).shuffle(10000, initial=10000).decode("pil").to_tuple("jpg", "cls").map_tuple(train_transform, lambda x: x)
+    train_loader = wds.WebLoader(train_dataset, batch_size=args.batch_size_per_gpu, num_workers=args.num_workers)
 
     val_dataset = ImageFolder(args.val_data_path, transform=val_transform)
     val_sampler = SequentialSampler(val_dataset)
     val_loader = DataLoader(val_dataset, sampler=val_sampler, batch_size=8*args.batch_size_per_gpu, num_workers=args.num_workers, pin_memory=True, drop_last=False)  # note we use a larger batch size for eval
 
-    print(f"Data loaded with {len(train_dataset)} train and {len(val_dataset)} val imgs.")
-    print(f"{len(train_loader)} train and {len(val_loader)} val iterations per epoch.")
+    print(f"Train and val data loaded.")
 
     # define the model
     model = tae.__dict__[args.model]()
@@ -121,93 +118,90 @@ def main(args):
 
     misc.load_model(args=args, model_without_ddp=model_without_ddp, optimizer=optimizer, loss_scaler=loss_scaler)
     
-    model.train(True)
+    model.train()
     metric_logger = misc.MetricLogger(delimiter="  ")
     optimizer.zero_grad()
 
     best_eval_loss = 100.0
 
     print("Starting TAE training!")
-    for epoch in range(args.start_epoch, args.epochs):
+    # infinite stream for iterable webdataset
+    for it, (samples, _) in enumerate(train_loader):
 
-        train_loader.sampler.set_epoch(epoch)
-        header = 'Epoch: [{}]'.format(epoch)
+        # optionally pick 8 examples for display and softmax estimation at regular intervals
+        if args.display and it % args.save_freq == 0:
+            samples_for_display_and_softmax = samples[:8, ...]
 
-        for it, (samples, _) in enumerate(metric_logger.log_every(train_loader, len(train_loader) // 1, header)):
+        samples = samples.to(device, non_blocking=True)
 
-            # TODO: push this shit to eval
-            if epoch == 0 and it == 0:
-                samples_for_display_and_softmax = samples[:8, ...]  # pick 8 examples for display and softmax estimation at each epoch end
+        with torch.cuda.amp.autocast():
+            loss, _ = model(samples)
 
-            samples = samples.to(device, non_blocking=True)
+        loss_value = loss.item()
 
-            with torch.cuda.amp.autocast():
-                loss, _ = model(samples)
+        if not math.isfinite(loss_value):
+            print("Loss is {}, stopping training".format(loss_value))
+            sys.exit(1)
 
-            loss_value = loss.item()
+        loss = loss / args.accum_iter
+        loss_scaler(loss, optimizer, parameters=model.parameters(), update_grad=(it + 1) % args.accum_iter == 0)
+        if (it + 1) % args.accum_iter == 0:
+            optimizer.zero_grad()
 
-            if not math.isfinite(loss_value):
-                print("Loss is {}, stopping training".format(loss_value))
-                sys.exit(1)
+        torch.cuda.synchronize()
 
-            loss = loss / args.accum_iter
-            loss_scaler(loss, optimizer, parameters=model.parameters(), update_grad=(it + 1) % args.accum_iter == 0)
-            if (it + 1) % args.accum_iter == 0:
-                optimizer.zero_grad()
+        metric_logger.update(loss=loss_value)
 
-            torch.cuda.synchronize()
+        if it != 0 and it % args.save_freq == 0:
+            # estimate eval loss
+            print(f"Iteration {it}, evaluating ...")
+            eval_loss = evaluate(val_loader, model_without_ddp, device)
 
-            metric_logger.update(loss=loss_value)
+            # save checkpoint only if eval_loss decreases
+            if eval_loss < best_eval_loss:
+                print("Best eval loss improved! Saving checkpoint.")
+                save_dict = {
+                    'model': model_without_ddp.state_dict(),
+                    'optimizer': optimizer.state_dict(),
+                    'args': args,
+                    'iteration': it,
+                    'scaler': loss_scaler.state_dict(),
+                }
 
-        # estimate eval loss at the end of epoch
-        eval_loss = evaluate(val_loader, model, device)
+                misc.save_on_master(save_dict, os.path.join(args.output_dir, f"{args.save_prefix}_checkpoint.pth"))
+                best_eval_loss = eval_loss
 
-        # save checkpoint if eval_loss decreases at the end of epoch
-        if eval_loss < best_eval_loss:
-            print("Best eval loss improved! Saving checkpoint.")
-            save_dict = {
-                'model': model_without_ddp.state_dict(),
-                'optimizer': optimizer.state_dict(),
-                'args': args,
-                'epoch': epoch,
-                'scaler': loss_scaler.state_dict(),
-            }
+            # gather the stats from all processes
+            metric_logger.synchronize_between_processes()
+            train_stats = {k: meter.global_avg for k, meter in metric_logger.meters.items()}
+            log_stats = {**{f'train_{k}': v for k, v in train_stats.items()}, 'eval_loss': eval_loss, 'iteration': it}
 
-            misc.save_on_master(save_dict, os.path.join(args.output_dir, f"{args.save_prefix}_checkpoint.pth"))
-            best_eval_loss = eval_loss
+            # write log
+            if misc.is_main_process():
+                with (Path(args.output_dir) / (args.save_prefix + "_log.txt")).open("a") as f:
+                    f.write(json.dumps(log_stats) + "\n")
 
-        # gather the stats from all processes
-        metric_logger.synchronize_between_processes()
-        train_stats = {k: meter.global_avg for k, meter in metric_logger.meters.items()}
-        log_stats = {**{f'train_{k}': v for k, v in train_stats.items()}, 'eval_loss': eval_loss, 'epoch': epoch}
+            # optionally display some reconstructions
+            if args.display:
+                with torch.no_grad():
+                    samples_for_display_and_softmax = samples_for_display_and_softmax.to(device, non_blocking=True)
+                    with torch.cuda.amp.autocast():
+                        _, pred = model_without_ddp(samples_for_display_and_softmax)
+                        pred = model_without_ddp.unpatchify(pred)
+                        
+                    combined = torch.cat((samples_for_display_and_softmax, pred), 0)
 
-        # write log
-        if misc.is_main_process():
-            with (Path(args.output_dir) / (args.save_prefix + "_log.txt")).open("a") as f:
-                f.write(json.dumps(log_stats) + "\n")
+                    # save original images and their reconstructions
+                    save_image(combined, os.path.join(args.output_dir, f"{args.save_prefix}_reconstructions_iter_{it}.jpg"), nrow=8, padding=1, normalize=True, scale_each=True)
 
-        # optionally display some reconstructions
-        if args.display:
-            with torch.no_grad():
-                samples_for_display_and_softmax = samples_for_display_and_softmax.to(device, non_blocking=True)
-                with torch.cuda.amp.autocast():
-                    _, pred = model(samples_for_display_and_softmax)
-                    pred = model_without_ddp.unpatchify(pred)
-                    
-                combined = torch.cat((samples_for_display_and_softmax, pred), 0)
+            # start a fresh logger to wipe off old stats
+            metric_logger = misc.MetricLogger(delimiter="  ")
 
-                # save original images and their reconstructions
-                save_image(combined, os.path.join(args.output_dir, f"{args.save_prefix}_reconstructions_epoch_{epoch}.jpg"), nrow=8, padding=1, normalize=True, scale_each=True)
-
-        # start a fresh logger to wipe off old stats
-        metric_logger = misc.MetricLogger(delimiter="  ")
-
-        # switch back to train mode
-        model.train(True)
+            # switch back to train mode, not 100% sure if this is strictly necessary since we're passing the unwrapped model to eval now
+            model.train()
 
 @torch.no_grad()
 def evaluate(data_loader, model, device):
-
     # switch to eval mode
     model.eval()
 
@@ -224,7 +218,7 @@ def evaluate(data_loader, model, device):
         eval_loss.append(loss_value)
 
     eval_loss = npmean(eval_loss)
-    print(f"Eval loss at the end of epoch: {eval_loss}")
+    print(f"Current eval loss: {eval_loss}")
 
     return eval_loss
 
