ca7b807e357ca64ebc2eb69e9bea82b42fbd9eb0
eminorhan <aeminorhan@gmail.com>
2024-05-13 18:04:42 -0400

    update train_recognition.sh (revert back to np based dataset loading)

--- scripts/train_recognition.sh
+++ scripts/train_recognition.sh
@@ -4,15 +4,11 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=16
-#SBATCH --mem=240GB
+#SBATCH --mem=750GB
 #SBATCH --time=2:00:00
 #SBATCH --job-name=train_recognition
 #SBATCH --output=train_recognition_%A_%a.out
-#SBATCH --array=2
-
-export MASTER_ADDR=$(hostname -s)
-export MASTER_PORT=$(shuf -i 10000-65500 -n 1)
-export WORLD_SIZE=1
+#SBATCH --array=1
 
 MODELS=(
 	tae_patch16_vocab16_px256
@@ -33,15 +29,18 @@ MODEL=${MODELS[$SLURM_ARRAY_TASK_ID]}
 
 # 21k
 srun python -u ../train_recognition.py \
-	--model vit_recognition_numpatches256_vocab256_small \
+	--model vit_recognition_numpatches256_vocab64_small \
 	--model_ckpt '' \
 	--num_classes 1000 \
 	--accum_iter 1 \
-	--batch_size 1024 \
+	--batch_size 256 \
+	--input_size 256 \
 	--lr 0.0001 \
 	--weight_decay 0.0 \
+	--save_freq 1000 \
 	--output_dir /scratch/eo41/tae/outputs_recognition/${MODEL} \
-	--data_path "/scratch/projects/lakelab/data_frames/imagenet-1k-processed/${MODEL}" \
+	--train_data_path "/scratch/projects/lakelab/data_frames/imagenet-1k-processed/${MODEL}/imagenet_1k_train_tae_patch16_vocab64_px256.pth" \
+	--val_data_path "/scratch/projects/lakelab/data_frames/imagenet-1k-processed/${MODEL}/imagenet_1k_val_tae_patch16_vocab64_px256.pth" \
 	--save_prefix imagenet_1k_${MODEL} \
 	--compile
 
